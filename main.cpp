39cd95b2da8716fe7f5b094136de3f57b4bd865f
Mira <mira@legendes-oubliees.org>
2025-06-02 11:43:04 +0200

    Fix index

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main() {
   Window root = Window();
   Dropdown *d = new Dropdown(&root, "placeholder" , {"1", "2", "3"});
   d->grid(0, 0);
-  (new Button(&root, "set to 1", [d](){d->set(1);}))->grid(0, 1);
+  (new Button(&root, "set to 1", [d](){d->set(0);}))->grid(0, 1);
   root.run();
 }
