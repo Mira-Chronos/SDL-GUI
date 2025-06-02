492bca1e07f0d1a7eedb2ed348870b38fce092d8
Mira <mira@legendes-oubliees.org>
2025-06-02 11:26:23 +0200

    Using just string

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include "Theme.hpp"
 int main() {
   Window root = Window();
-  Dropdown *d = new Dropdown(&root, L"placeholder" , {L"1", L"2", L"3"});
+  Dropdown *d = new Dropdown(&root, "placeholder" , {"1", "2", "3"});
   d->grid(0, 0);
-  (new Button(&root, L"set to 1", [d](){d->set(1);}))->grid(0, 1);
+  (new Button(&root, "set to 1", [d](){d->set(1);}))->grid(0, 1);
   root.run();
 }
